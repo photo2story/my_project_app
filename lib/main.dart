d9c65fd3376ff7fcd516742f7e453abfbd8e89bf
github.com:photo2story <waterlee.kr@gmail.com>
2025-01-09 23:46:01 +0900

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
           ),
         ),
       ),
-      home: const DashboardScreen(),
+      home: DashboardScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import '../models/project.dart';
 
 class DashboardScreen extends StatelessWidget {
+  const DashboardScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
